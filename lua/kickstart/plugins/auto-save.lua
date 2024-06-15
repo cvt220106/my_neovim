1e9e14b6ab9eb1793479b2f5dc22ce8c2ec5fb83
cvt220106 <1564851464@qq.com>
2024-06-16 01:27:26 +0800

    add new auto-save plugins and activate it

---
+++ lua/kickstart/plugins/auto-save.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'okuuva/auto-save.nvim',
+    event = { 'BufReadPre' },
+    opts = {
+      events = { 'InsertLeave', 'BufLeave' },
+      silent = false,
+      exclude_ft = { 'neo-tree' },
+    },
+  },
+}
